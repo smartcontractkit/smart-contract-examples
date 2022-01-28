6effa45b11f8c1c5cf38c78ef8ec2fe385a639a0
Richard Gottleber <richard@gottleber.net>
2022-01-28 12:19:37 -0600

    create sample project for front end price feeds

---
+++ pricefeeds-in-svelte/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+    import { getETHPrice } from "../utils/getETHPrice";
+    let value;
+    onMount(async () => {
+        value = await getETHPrice();
+    });
+</script>
+
+<h1>Current ETH Price in USD</h1>
+<p>
+    $ {(1 * value).toFixed(2)} USD
+</p>
