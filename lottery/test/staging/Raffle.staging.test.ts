5a675606a81496260e2143dc7fe085673b514cf6
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-04 22:45:05 -0400

    fixing typos in Raffle.tests.ts

--- lottery/test/staging/Raffle.staging.test.ts
+++ lottery/test/staging/Raffle.staging.test.ts
@@ -1,1 +1,65 @@
-// accepting PRs to update this :) 
+import{ assert, expect } from "chai"
+import { getNamedAccounts, deployments, ethers, network } from "hardhat"
+import { developmentChains, networkConfig } from "../../helper-hardhat-config"
+import { BigNumber } from "ethers"
+import { Raffle } from "../../typechain-types"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", function () {
+      let raffle: Raffle
+      let raffleEntranceFee: BigNumber
+      let deployer: SignerWithAddress
+
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              raffle = await ethers.getContract("Raffle", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+          })
+
+          describe("fulfillRandomWords", function () {
+              it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function () {
+                  // enter the raffle
+                  console.log("Setting up test...")
+                  const startingTimeStamp = await raffle.getLatestTimeStamp()
+                  const accounts = await ethers.getSigners()
+
+                  console.log("Setting up Listener...")
+                  await new Promise(async (resolve, reject) => {
+                      // setup listener before we enter the raffle
+                      // Just in case the blockchain moves REALLY fast
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("WinnerPicked event fired!")
+                          try {
+                              // add our asserts here
+                              const recentWiner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const winnerEndingBalance = await accounts[0].getBalance()
+                              const endingTimeStamp = await raffle.getLatestTimeStamp()
+
+                              await expect(raffle.getPlayer(0)).to.be.reverted
+                              assert.equal(recentWinner.toString(), accounts[0].address)
+                              assert.equal(raffleState, 0)
+                              assert.equal(
+                                  winnerEndingBalance.toString(),
+                                  winnerStartingBalance.add(raffleEntranceFee).toString()
+                              )
+                              assert(endingTimeStamp > startingTimeStamp)
+                              resolve()
+                          } catch (error) {
+                              console.log(error)
+                              reject(e)
+                          }
+                      })
+                      // Then entering the raffle
+                      console.log("Entering Raffle...")
+                      await raffle.enterRaffle({ value: raffleEntranceFee })
+                      console.log("Ok, time to wait...")
+                      const winnerStartingBalance = await accounts[0].getBalance()
+
+                      // and this code WONT complete until our listener has finished listening!
+                  })
+              })
+          })
+      })
