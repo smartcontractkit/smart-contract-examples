f38c6459d26dbe03214e820d2db153663e8206fe
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-04 23:46:55 -0400

    added ts staging test for Raffle.sol

--- lottery/test/unit/Raffle.test.ts
+++ lottery/test/unit/Raffle.test.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { assert, expect } from "chai"
 import { BigNumber } from "ethers"
-import { network, deployments, ethers, getNamedAccounts } from "hardhat"
+import { network, deployments, ethers } from "hardhat"
 import { developmentChains, networkConfig } from "../../helper-hardhat-config"
 import { Raffle, VRFCoordinatorV2Mock } from "../../typechain-types"
 
