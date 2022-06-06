1561102edc0e46252f06eb866b579ad782ab81ff
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-06 02:33:25 -0400

    added comments

--- lottery/helper-hardhat-config.ts
+++ lottery/helper-hardhat-config.ts
@@ -30,7 +30,7 @@ export const networkConfig: networkConfigInfo = {
     },
     4: {
         name: "rinkeby",
-        subscriptionId: "5864",
+        subscriptionId: "588",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         charityRaffleDuration: "30", // 30 seconds (just for testing)
