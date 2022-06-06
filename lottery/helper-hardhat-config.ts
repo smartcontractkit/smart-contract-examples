e6ddaa75e7b406cfa3a9065a4fc79d8c8f3c72cc
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-06 03:47:27 -0400

    added enter scripts

--- lottery/helper-hardhat-config.ts
+++ lottery/helper-hardhat-config.ts
@@ -30,7 +30,7 @@ export const networkConfig: networkConfigInfo = {
     },
     4: {
         name: "rinkeby",
-        subscriptionId: "588",
+        subscriptionId: "5864",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         charityRaffleDuration: "30", // 30 seconds (just for testing)
@@ -38,9 +38,9 @@ export const networkConfig: networkConfigInfo = {
         jackpot: "200000000000000000", // 0.2 ETH
         callbackGasLimit: "500000", // 500,000 gas
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        charity1: "", // use extra wallet accounts to test
-        charity2: "",
-        charity3: "",
+        charity1: "0x8423f6c5f0895914e0C8A4eF523C0A1d5c8632f6", // use extra wallet accounts to test
+        charity2: "0x70185775Ae9767751c218d9baAeffBC9b5fD5b34",
+        charity3: "0xa95224aE036279f0f2A07623D94F44fDb03F1C45",
     },
     1: {
         name: "mainnet",
--- lottery/scripts/enterRaffle.ts
+++ lottery/scripts/enterRaffle.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
 import { ethers } from "hardhat"
+import { Raffle } from '../typechain-types'
 
-async function enterRaffle() {
-    const raffle = await ethers.getContract("Raffle")
-    const entranceFee = await raffle.getEntranceFee()
+async function enterRaffle():Promise<void> {
+    const raffle: Raffle = await ethers.getContract("Raffle")
+    const entranceFee: number = (await raffle.getEntranceFee()).toNumber()
     await raffle.enterRaffle({ value: entranceFee + 1 })
     console.log("Entered!")
 }
