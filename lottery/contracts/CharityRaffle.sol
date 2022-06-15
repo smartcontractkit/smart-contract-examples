c4c945746490eb7de1f4fce51fcbeb14a003d39a
Kate Johnson <91970214+K-G-J@users.noreply.github.com>
2022-06-15 17:44:09 -0400

    Update lottery/contracts/CharityRaffle.sol
    Co-authored-by: Andrej <andrej.rakic@elfak.rs>

--- lottery/contracts/CharityRaffle.sol
+++ lottery/contracts/CharityRaffle.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
-import "hardhat/console.sol";
 
 error CharityRaffle__NotJackpotValue();
 error CharityRaffle__FundingContractFailed();
