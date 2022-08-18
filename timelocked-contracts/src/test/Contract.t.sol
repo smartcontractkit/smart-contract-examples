4533bda9abfd115760216f671ee1d9f7686200be
Richard Gottleber <richard@gottleber.net>
2022-08-18 13:57:40 -0500

    Create contract and tests

--- timelocked-contracts/src/test/Contract.t.sol
+++ timelocked-contracts/src/test/Contract.t.sol
@@ -2,11 +2,119 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "../Contract.sol";
+
+interface CheatCodes {
+    function warp(uint256) external;
+}
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    CheatCodes constant cheats = CheatCodes(HEVM_ADDRESS);
 
-    function testExample() public {
-        assertTrue(true);
+    Contract public c;
+
+    function setUp() public {
+        c = new Contract();
+        c.queueMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp + 600
+        );
+    }
+
+    function testFailMintNow() public {
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp + 600
+        );
+    }
+
+    function testFailDoubleQueue() public {
+        c.queueMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp + 600
+        );
+    }
+
+    function testFailMintNonQueued() public {
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            999,
+            block.timestamp + 600
+        );
+    }
+
+    function testMintAfterTen() public {
+        uint256 targetTime = block.timestamp + 600;
+        cheats.warp(block.timestamp + 600);
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            targetTime
+        );
+    }
+
+    function testFailDoubleMint() public {
+        uint256 targetTime = block.timestamp + 600;
+        cheats.warp(block.timestamp + 600);
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            targetTime
+        );
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp + 600
+        );
+    }
+
+    function testFailLateMint() public {
+        uint256 targetTime = block.timestamp + 600;
+        cheats.warp(block.timestamp + 600 + 1801);
+        emit log_uint(block.timestamp);
+        c.executeMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            targetTime
+        );
+    }
+
+    function testCancelMint() public {
+        bytes32 txnHash = c.generateTxnHash(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp + 600
+        );
+        c.cancelMint(txnHash);
+    }
+
+    function testFailCancelMint() public {
+        bytes32 txnHash = c.generateTxnHash(
+            0x1234567890123456789012345678901234567890,
+            999,
+            block.timestamp + 600
+        );
+        c.cancelMint(txnHash);
+        c.cancelMint(txnHash);
+    }
+
+    function testFailCancelMintNonQueued() public {
+        bytes32 txnHash = c.generateTxnHash(
+            0x1234567890123456789012345678901234567890,
+            999,
+            block.timestamp + 600
+        );
+        c.cancelMint(txnHash);
+    }
+
+    function testFailPastQueue() public {
+        c.queueMint(
+            0x1234567890123456789012345678901234567890,
+            100,
+            block.timestamp - 600
+        );
     }
 }
