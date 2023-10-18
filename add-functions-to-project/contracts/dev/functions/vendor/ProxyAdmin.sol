019e3f87efa9fe3edfb9d560f421b71520714652
Dwight Lyle <dwightjl@gmail.com>
2023-10-18 10:37:08 -0600

    Remove outdated Functions example

--- add-functions-to-project/contracts/dev/functions/vendor/ProxyAdmin.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.1 (proxy/transparent/ProxyAdmin.sol)
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
