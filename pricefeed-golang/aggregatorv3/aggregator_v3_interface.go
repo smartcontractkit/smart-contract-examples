925aeac8e2f6f86457c425fb4c208d87a6a18fa2
aelmanaa <aelmanaa@gmail.com>
2024-03-06 16:57:27 +0100

    fix pricefeed-golang tutorial

--- pricefeed-golang/aggregatorv3/aggregator_v3_interface.go
+++ pricefeed-golang/aggregatorv3/aggregator_v3_interface.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // AggregatorV3InterfaceMetaData contains all meta data concerning the AggregatorV3Interface contract.
@@ -134,11 +135,11 @@ func NewAggregatorV3InterfaceFilterer(address common.Address, filterer bind.Cont
 
 // bindAggregatorV3Interface binds a generic wrapper to an already deployed contract.
 func bindAggregatorV3Interface(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(AggregatorV3InterfaceABI))
+	parsed, err := AggregatorV3InterfaceMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
