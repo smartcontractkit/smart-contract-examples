4d309eb9400d3b30addfa5ff289b8846e1852352
Amine E <aelmanaa@users.noreply.github.com>
2024-03-18 06:49:36 +0100

    add comments (#49)

--- ccip/estimate-gas/foundry/test/SendReceive.t.sol
+++ ccip/estimate-gas/foundry/test/SendReceive.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+// Importing necessary components from the Chainlink and Forge Standard libraries for testing.
 import {Test, console, Vm} from "forge-std/Test.sol";
 import {BurnMintERC677} from "@chainlink/contracts-ccip/src/v0.8/shared/token/ERC677/BurnMintERC677.sol";
 import {MockCCIPRouter} from "@chainlink/contracts-ccip/src/v0.8/ccip/test/mocks/MockRouter.sol";
@@ -8,31 +9,41 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {Sender} from "../src/Sender.sol";
 import {Receiver} from "../src/Receiver.sol";
 
+/// @title A test suite for Sender and Receiver contracts to estimate ccipReceive gas usage.
 contract SenderReceiverTest is Test {
+    // Declaration of contracts and variables used in the tests.
     Sender public sender;
     Receiver public receiver;
     BurnMintERC677 public link;
     MockCCIPRouter public router;
+    // A specific chain selector for identifying the chain.
     uint64 public chainSelector = 16015286601757825753;
 
+    /// @dev Sets up the testing environment by deploying necessary contracts and configuring their states.
     function setUp() public {
+        // Mock router and LINK token contracts are deployed to simulate the network environment.
         router = new MockCCIPRouter();
         link = new BurnMintERC677("ChainLink Token", "LINK", 18, 10 ** 27);
+        // Sender and Receiver contracts are deployed with references to the router and LINK token.
         sender = new Sender(address(router), address(link));
         receiver = new Receiver(address(router));
+        // Configuring allowlist settings for testing cross-chain interactions.
         sender.allowlistDestinationChain(chainSelector, true);
         receiver.allowlistSourceChain(chainSelector, true);
         receiver.allowlistSender(address(sender), true);
     }
 
+    /// @dev Helper function to simulate sending a message from Sender to Receiver.
+    /// @param iterations The variable to simulate varying loads in the message.
     function sendMessage(uint256 iterations) private {
-        vm.recordLogs();
+        vm.recordLogs(); // Starts recording logs to capture events.
         sender.sendMessagePayLINK(
             chainSelector,
             address(receiver),
             iterations,
-            400000
+            400000 // A predefined gas limit for the transaction.
         );
+        // Fetches recorded logs to check for specific events and their outcomes.
         Vm.Log[] memory logs = vm.getRecordedLogs();
         bytes32 msgExecutedSignature = keccak256(
             "MsgExecuted(bool,bytes,uint256)"
@@ -53,44 +64,18 @@ contract SenderReceiverTest is Test {
         }
     }
 
+    /// @notice Test case for the minimum number of iterations.
     function test_SendReceiveMin() public {
         sendMessage(0);
     }
 
+    /// @notice Test case for an average number of iterations.
     function test_SendReceiveAverage() public {
         sendMessage(50);
     }
 
+    /// @notice Test case for the maximum number of iterations.
     function test_SendReceiveMax() public {
         sendMessage(99);
     }
-
-    /*
-    function test_SendReceiveDirect() public {
-        Client.Any2EVMMessage memory executableMsg = Client.Any2EVMMessage({
-            messageId: bytes32(uint256(123)),
-            sourceChainSelector: chainSelector,
-            sender: abi.encode(address(sender)),
-            data: abi.encode(10),
-            destTokenAmounts: new Client.EVMTokenAmount[](0)
-        });
-        uint256 intrinsicGas = calculateIntrinsicGas(abi.encode(executableMsg));
-
-        vm.prank(address(router));
-        receiver.ccipReceive(executableMsg);
-
-        console.log("Intrinsic Gas:", intrinsicGas);
-    }
-
-    function calculateIntrinsicGas(
-        bytes memory data
-    ) public pure returns (uint256) {
-        uint256 gas = 0;
-        for (uint i = 0; i < data.length; i++) {
-            // 4 gas for a zero byte, 16 gas for a non-zero byte
-            gas += data[i] == bytes1(0) ? 4 : 16;
-        }
-        return 21000 + gas;
-    }
-    */
 }
--- ccip/estimate-gas/hardhat/test/Send-Receive.ts
+++ ccip/estimate-gas/hardhat/test/Send-Receive.ts
@@ -1,10 +1,14 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { ethers } from "hardhat";
 
+// Define a test suite for Sender and Receiver contracts.
 describe("Sender and Receiver", function () {
+  // Define a chain selector for the test scenario.
   const chainSelector = "16015286601757825753";
 
+  // A fixture to deploy necessary contracts before each test.
   async function deployFixture() {
+    // Get signers, with the first one typically being the deployer.
     const [owner] = await ethers.getSigners();
 
     const Router = await ethers.getContractFactory("MockCCIPRouter");
@@ -12,6 +16,7 @@ describe("Sender and Receiver", function () {
     const Receiver = await ethers.getContractFactory("Receiver");
     const BurnMintERC677 = await ethers.getContractFactory("BurnMintERC677");
 
+    // Instantiate the contracts.
     const router = await Router.deploy();
     const link = await BurnMintERC677.deploy(
       "ChainLink Token",
@@ -19,26 +24,29 @@ describe("Sender and Receiver", function () {
       18,
       BigInt(1e27)
     );
-
     const sender = await Sender.deploy(router, link);
     const receiver = await Receiver.deploy(router);
-    await sender.allowlistDestinationChain(chainSelector, true);
 
+    // Setup allowlists for chains and sender addresses for the test scenario.
+    await sender.allowlistDestinationChain(chainSelector, true);
     await receiver.allowlistSourceChain(chainSelector, true);
     await receiver.allowlistSender(sender, true);
 
+    // Return the deployed instances and the owner for use in tests.
     return { owner, sender, receiver, router, link };
   }
 
+  // Test scenario to send a CCIP message from sender to receiver and assess gas usage.
   it("should CCIP message from sender to receiver", async function () {
+    // Deploy contracts and load their instances.
     const { sender, receiver, router } = await loadFixture(deployFixture);
+
+    // Define parameters for the tests, including gas limit and iterations for messages.
     const gasLimit = 400000;
-    const testParams = [
-      0, // min
-      50, // average
-      99, // max
-    ];
-    const gasUsageReport = [];
+    const testParams = [0, 50, 99]; // Different iteration values for testing.
+    const gasUsageReport = []; // To store reports of gas used for each test.
+
+    // Loop through each test parameter to send messages and record gas usage.
     for (const iterations of testParams) {
       await sender.sendMessagePayLINK(
         chainSelector,
@@ -47,20 +55,21 @@ describe("Sender and Receiver", function () {
         gasLimit
       );
 
-      // check gas used
+      // Retrieve gas used from the last message executed by querying the router's events.
       const mockRouterEvents = await router.queryFilter(
         router.filters.MsgExecuted
       );
       const mockRouterEvent = mockRouterEvents[mockRouterEvents.length - 1]; // check last event
       const gasUsed = mockRouterEvent.args.gasUsed;
 
+      // Push the report of iterations and gas used to the array.
       gasUsageReport.push({
         iterations,
         gasUsed: gasUsed.toString(),
       });
     }
 
-    // Output the final report
+    // Log the final report of gas usage for each iteration.
     console.log("Final Gas Usage Report:");
     gasUsageReport.forEach((report) => {
       console.log(
