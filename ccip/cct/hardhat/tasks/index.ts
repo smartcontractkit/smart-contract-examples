471677774e68ba7a1ffb019aba2cd9ee5f75fae9
aelmanaa <aelmanaa@gmail.com>
2024-12-02 23:44:30 +0100

    update

--- ccip/cct/hardhat/tasks/index.ts
+++ ccip/cct/hardhat/tasks/index.ts
@@ -14,3 +14,5 @@ import "./getCurrentRateLimits";
 import "./setRateLimitAdmin";
 import "./acceptAdminRole";
 import "./transferTokenAdminRole";
+import "./addRemotePool";
+import "./removeRemotePool";
